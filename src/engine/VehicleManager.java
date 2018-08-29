572b25ff5fdbd5e7e3118a684bfd1e38d5559da1
Aniket Kumar Tripathi <aniketkumartripathi@gmail.com>
2018-08-29 22:25:10 +0530

    Add methods for pool management
    Methods include empty check, add vehicle to pool and get vehicle from
    pool for different types of vehicles.

--- src/engine/VehicleManager.java
+++ src/engine/VehicleManager.java
@@ -1,6 +1,99 @@
 package engine;
 
+import java.util.LinkedList;
+
+import entities.Car;
+import entities.HeavyVehicle;
+import entities.TwoWheeler;
+import entities.Vehicle;
+
 public class VehicleManager {
 		
+	private LinkedList<Car> carPool;
+	private LinkedList<TwoWheeler> twoWheelerPool;
+	private LinkedList<HeavyVehicle> heavyVehiclePool;
+	
+	
+	public VehicleManager() {
+		
+		carPool = new LinkedList<Car>();
+		twoWheelerPool = new LinkedList<TwoWheeler>();
+		heavyVehiclePool = new LinkedList<HeavyVehicle>();
+		
+	}
+	
+	
+	public boolean isCarPoolEmpty() {
+		return carPool.isEmpty();
+	}
+	
+	
+	public boolean isTwoWheelerPoolEmpty() {
+		return twoWheelerPool.isEmpty();
+	}
+	
+	public boolean isHeavyVehiclePoolEmpty() {
+		return heavyVehiclePool.isEmpty();
+	}
+	
+	
+	
+	public Car getCar() {
+		Car car;
+		
+		if(isCarPoolEmpty())
+			car =  new Car();
+		
+		else {
+			car = carPool.pollFirst();
+		}
+		
+		return car;
+	}
+	
+	
+	public TwoWheeler getTwoWheeler() {
+		TwoWheeler twoWheeler;
+		
+		if(isCarPoolEmpty())
+			twoWheeler =  new TwoWheeler();
+		
+		else {
+			twoWheeler = twoWheelerPool.pollFirst();
+		}
+		
+		return twoWheeler;
+	}
+	
+	
+	public HeavyVehicle getHeavyVehicle() {
+		HeavyVehicle heavyVehicle;
+		
+		if(isCarPoolEmpty())
+			heavyVehicle =  new HeavyVehicle();
+		
+		else {
+			heavyVehicle = heavyVehiclePool.pollFirst();
+		}
+		
+		return heavyVehicle;
+	}
+	
+	
+	public void addToPool(Vehicle vehicle) {
+		vehicle.reset();
+		if(vehicle.getClass() == Car.class) {
+			carPool.addLast((Car)vehicle);
+		}
+		else if(vehicle.getClass() == TwoWheeler.class) {
+			twoWheelerPool.addLast((TwoWheeler) vehicle);
+		}
+		else if(vehicle.getClass() == HeavyVehicle.class) {
+			heavyVehiclePool.addLast((HeavyVehicle)vehicle);
+		}
+		
+		
+	}
+	
 	
 }
