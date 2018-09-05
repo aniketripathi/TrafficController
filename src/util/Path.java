75c1697086b96c28904ce9f18269e5a040cc4ade
Aniket Kumar Tripathi <aniketkumartripathi@gmail.com>
2018-09-05 21:31:40 +0530

    Add path class
    The path class is a represenation of bezier curve.

---
+++ src/util/Path.java
@@ -0,0 +1,12 @@
+package util;
+
+public abstract class Path {
+
+	/** Defined by Bezier curve B(r) **/
+	
+	public abstract double getX(double r);
+	
+	public abstract double getY(double r);
+	
+	
+}
