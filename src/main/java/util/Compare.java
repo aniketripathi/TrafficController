d19a0b46d2602d5f319612ed4023456a4e17ec7c
Aniket Kumar Tripathi <aniketkumartripathi@gmail.com>
2018-09-18 03:03:08 +0530

    Add more methods for equal comparison

--- src/main/java/util/Compare.java
+++ src/main/java/util/Compare.java
@@ -16,12 +16,19 @@ public class Compare {
 	}
 
 	public static boolean isSmaller(double a, double b) {
-		return !(isLarger(a, b) || isEqual(a, b));
+		return ((b-a) > THRESHOLD);
 	}
 
 	public static boolean isEqual(double a, double b) {
 		return (Math.abs(a - b) < THRESHOLD);
 	}
 
+	public static boolean isSmallerEquals(double a, double b) {
+		return (isSmaller(a,b) || isEqual(a,b));
+	}
+	
+	public static boolean isLargerEqual(double a, double b) {
+		return (isLarger(a,b) || isEqual(a,b));
+	}
 	
 }
