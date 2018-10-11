aeda54751eca3ba318ec39b98ee18b3e44caa9d0
Aniket Kumar Tripathi <aniketkumartripathi@gmail.com>
2018-10-12 05:24:38 +0530

    Add getDistance() method

--- src/main/java/util/MathEngine.java
+++ src/main/java/util/MathEngine.java
@@ -1,22 +1,24 @@
 package main.java.util;
 
-public class Compare {
-	
+public class MathEngine {
+
+	public static final double NINETY_ANGLE_CLOCKWISE = 90;
+	public static final double NINETY_ANGLE_ANTICLOCKWISE = -90;
+
 	/**
 	 * The maximum difference value tolerated when comparing doubles. Thus if x and
 	 * y are not equal only if they differ more than the threshold value.
 	 */
 
 	public static final double THRESHOLD = 0.00001;
-	
-	
+
 	public static boolean isLarger(double a, double b) {
 		return ((a - b) > THRESHOLD);
 
 	}
 
 	public static boolean isSmaller(double a, double b) {
-		return ((b-a) > THRESHOLD);
+		return ((b - a) > THRESHOLD);
 	}
 
 	public static boolean isEqual(double a, double b) {
@@ -24,11 +26,15 @@ public class Compare {
 	}
 
 	public static boolean isSmallerEquals(double a, double b) {
-		return (isSmaller(a,b) || isEqual(a,b));
+		return (isSmaller(a, b) || isEqual(a, b));
 	}
-	
-	public static boolean isLargerEqual(double a, double b) {
-		return (isLarger(a,b) || isEqual(a,b));
+
+	public static boolean isLargerEquals(double a, double b) {
+		return (isLarger(a, b) || isEqual(a, b));
 	}
-	
+
+	public static double getDistance(double x, double y) {
+		return Math.sqrt(x * x + y * y);
+	}
+
 }
