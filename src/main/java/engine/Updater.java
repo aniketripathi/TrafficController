6eef955cd21888227b0f67cdb60f292737d22c0a
Aniket Kumar Tripathi <aniketkumartripathi@gmail.com>
2018-10-27 15:24:38 +0530

    Add generation algorithm and integration
    Add vehicle generation algorithm
    Integration of Updater with Config
    Integration of Updater with Recorder
    Add Countdown timers

--- src/main/java/engine/Updater.java
+++ src/main/java/engine/Updater.java
@@ -1,18 +1,23 @@
 package main.java.engine;
 
-import java.util.LinkedList;
 import java.util.ListIterator;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import main.java.data.Config;
 import main.java.data.Recorder;
-import main.java.entities.Car;
-import main.java.entities.HeavyVehicle;
+import main.java.entities.BackwardLane;
+import main.java.entities.ForwardLane;
+import main.java.entities.Road;
 import main.java.entities.Vehicle;
+import main.java.entities.Vehicle.Type;
 import main.java.map.Map;
+import main.java.util.CountdownTimer;
+import main.java.util.CountdownTimerEvent;
+import main.java.util.MathEngine;
 import main.java.util.Timer;
 
 public class Updater implements ChangeListener<Number> {
@@ -23,40 +28,49 @@ public class Updater implements ChangeListener<Number> {
 	private Config config;
 	private Recorder recorder;
 	private RuleSet ruleSet;
+	private CountdownTimer trafficTimer;
 	private Timer timer;
+	private CountdownTimer[][] vehicleGenerationTimer;
+	private boolean[][] generateVehicle;
+	private GenerateVehicleEvent[][] generateVehicleEvent;
+
+	private boolean initialized;
+	private boolean isStopped;
+	private boolean isPaused;
+
 	private Label label;
 
 	double canvasWidth;
 	double canvasTranslateY = 0;
-	int counter = 0;
-	int timer1 = 10;
-	private RoadVehicle roads[];
 
-	class RoadVehicle {
+	private class GenerateVehicleEvent implements CountdownTimerEvent {
 
-		int numberOfLanes;
-		LinkedList<Vehicle> forwardLanes[];
-		LinkedList<Vehicle> backwardLanes[];
+		private final int i;
+		private final int j;
 
-		@SuppressWarnings("unchecked")
-		public RoadVehicle(int numberOfLanes) {
-
-			forwardLanes = new LinkedList[numberOfLanes];
-			backwardLanes = new LinkedList[numberOfLanes];
-			for (int i = 0; i < numberOfLanes; i++) {
-				forwardLanes[i] = new LinkedList<Vehicle>();
-				backwardLanes[i] = new LinkedList<Vehicle>();
-			}
+		public GenerateVehicleEvent(int road, int lane) {
+			super();
+			i = road;
+			j = lane;
 		}
 
+		@Override
+		public void run() {
+			setGenerateVehicle(i, j, true);
+
+		}
 	}
 
 	public Updater(Map map, Canvas canvas, VehicleManager vehicleManager, Config config, Recorder recorder,
 			Label timerValueLabel) {
 
-		roads = new RoadVehicle[Map.NUMBER_OF_ROADS];
 		this.label = timerValueLabel;
 		this.timer = new Timer();
+		trafficTimer = new CountdownTimer(1);
+		vehicleGenerationTimer = new CountdownTimer[Map.NUMBER_OF_ROADS][map.getNumberOfLanes()];
+		generateVehicle = new boolean[Map.NUMBER_OF_ROADS][map.getNumberOfLanes()];
+		generateVehicleEvent = new GenerateVehicleEvent[Map.NUMBER_OF_ROADS][map.getNumberOfLanes()];
+
 		this.config = config;
 		this.recorder = recorder;
 		this.map = map;
@@ -66,69 +80,152 @@ public class Updater implements ChangeListener<Number> {
 		canvas.widthProperty().addListener(this);
 		canvas.heightProperty().addListener(this);
 
-		for (int j = 0; j < Map.NUMBER_OF_ROADS; j++) {
-			roads[j] = new RoadVehicle(map.getNumberOfLanes());
+		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+			for (int j = 0; j < map.getNumberOfLanes(); j++) {
+				setGenerateVehicle(i, j, true);
+				vehicleGenerationTimer[i][j] = new CountdownTimer(CountdownTimer.INFINITE);
+				generateVehicleEvent[i][j] = new GenerateVehicleEvent(i, j);
+				vehicleGenerationTimer[i][j].setCountdown(
+						(long) config.getRoadProperty(i).getLaneProperty(j).getRate() * CountdownTimer.SECOND_TO_NANOS,
+						generateVehicleEvent[i][j]);
+			}
 		}
+
+		trafficTimer.setCountdown(50 * Timer.SECOND_TO_NANOS,
+				map.getRoad(2).getTrafficLight().getCountdownTimerEvent());
+		initialize();
 
 	}
 
-	public Timer getTimer() {
-		return timer;
+	private synchronized boolean getGenerateVehicle(int i, int j) {
+		return generateVehicle[i][j];
+	}
+
+	private synchronized void setGenerateVehicle(int i, int j, boolean value) {
+		generateVehicle[i][j] = value;
+	}
+
+	public void doubleRate() {
+		timer.doubleRate();
+		trafficTimer.doubleRate();
+		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+			for (int j = 0; j < map.getNumberOfLanes(); j++) {
+				vehicleGenerationTimer[i][j].doubleRate();
+			}
+		}
+	}
+
+	public void halfRate() {
+		timer.halfRate();
+		trafficTimer.halfRate();
+		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+			for (int j = 0; j < map.getNumberOfLanes(); j++) {
+				vehicleGenerationTimer[i][j].doubleRate();
+			}
+		}
 	}
 
 	public void update() {
-		if (counter <= 50 && timer1 % 18 == 0) {
-			generateVehicle();
-			++counter;
+
+		/** Vehicle Generation **/
+		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+			for (int j = 0; j < map.getNumberOfLanes(); j++) {
+				if (getGenerateVehicle(i, j)) {
+					generateVehicle(i, j);
+					setGenerateVehicle(i, j, false);
+				}
+			}
 		}
-		vehicleUpdate();
-		timer1++;
-		label.setText(Double.toString(this.getTimer().getElapsedNanos() / 1_000_000_000).toString());
+		updateVehicles();
+
+		label.setText(Double.toString(timer.getElapsedNanos() / (double) 1_000_000_000).toString());
+
 	}
 
-	private void vehicleUpdate() {
-		for (RoadVehicle roadVehicle : roads) { // for all roads
+	private void updateVehicles() {
+		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
 
-			for (LinkedList<Vehicle> lane : roadVehicle.backwardLanes) { // for all backwardLanes
-
-				ListIterator<Vehicle> iterator = lane.listIterator(lane.size());
+			for (int j = 0; j < map.getNumberOfLanes(); j++) {
+				BackwardLane lane = map.getRoad(i).getBackwardLane(j);
+				ListIterator<Vehicle> iterator = lane.listIterator();
 				Vehicle nextVehicle = null;
-				if (iterator.hasPrevious()) {
+				if (iterator.hasNext()) {
 					Vehicle vehicle = iterator.next();
 					boolean beDestroyed = vehicle.update(nextVehicle, true);
 					nextVehicle = vehicle;
 					if (beDestroyed) {
 						iterator.remove();
 						vehicleManager.addToPool(vehicle);
+						recorder.getRoadCount(i).getBackwardLaneCount(j).getDestroyedCount()
+								.incrementCount(vehicle.getType());
+						recorder.getRoadCount(i).getBackwardLaneCount(j).getInLaneCount()
+								.decrementCount(vehicle.getType());
 					}
 				}
-
-				while (iterator.hasPrevious()) {
-					Vehicle vehicle = iterator.previous();
+				while (iterator.hasNext()) {
+					Vehicle vehicle = iterator.next();
 					boolean beDestroyed = vehicle.update(nextVehicle, false);
 					nextVehicle = vehicle;
 					if (beDestroyed) {
 						iterator.remove();
 						vehicleManager.addToPool(vehicle);
+						recorder.getRoadCount(i).getBackwardLaneCount(j).getDestroyedCount()
+								.incrementCount(vehicle.getType());
+						recorder.getRoadCount(i).getBackwardLaneCount(j).getInLaneCount()
+								.decrementCount(vehicle.getType());
 					}
 				}
 			}
 
-			for (LinkedList<Vehicle> lane : roadVehicle.forwardLanes) { // for all forwardLanes
+		}
 
-				ListIterator<Vehicle> iterator = lane.listIterator(lane.size());
+		for (int i = 0; i < map.getCrossing().getQueueSize(); i++) {
+			ListIterator<Vehicle> iterator = map.getCrossing().listIterator();
+			while (iterator.hasNext()) {
+				Vehicle vehicle = iterator.next();
+				boolean leftCrossing = vehicle.update(null, false);
+				if (leftCrossing) {
+					iterator.remove();
+					vehicle.getDestinationLane().addVehicle(vehicle);
+					recorder.getCrossingCount().getInCrossingCount().decrementCount(vehicle.getType());
+					recorder.getCrossingCount().getTotalCrossedCount().incrementCount(vehicle.getType());
+					recorder.getCrossingCount()
+							.getCrossingDetailCount(vehicle.getSourceRoad().getIndex(),
+									vehicle.getSourceLane().getIndex(), vehicle.getDestinationRoad().getIndex(),
+									vehicle.getDestinationLane().getIndex())
+							.incrementCount(vehicle.getType());
+
+				}
+			}
+
+		}
+
+		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+
+			for (int j = 0; j < map.getNumberOfLanes(); j++) {
+				ForwardLane lane = map.getRoad(i).getForwardLane(j);
+				ListIterator<Vehicle> iterator = lane.listIterator();
 				Vehicle nextVehicle = null;
-
-				if (iterator.hasPrevious()) {
-					Vehicle vehicle = iterator.previous();
-					vehicle.update(nextVehicle, true);
+				if (iterator.hasNext()) {
+					Vehicle vehicle = iterator.next();
+					boolean enterCrossing = vehicle.update(nextVehicle, true);
 					nextVehicle = vehicle;
+					if (enterCrossing) {
+						recorder.getCrossingCount().getInCrossingCount().decrementCount(vehicle.getType());
+						iterator.remove();
+						map.getCrossing().addVehicle(vehicle);
+					}
 				}
 
-				while (iterator.hasPrevious()) {
-					Vehicle vehicle = iterator.previous();
-					vehicle.update(nextVehicle, false);
+				while (iterator.hasNext()) {
+					Vehicle vehicle = iterator.next();
+					boolean enterCrossing = vehicle.update(nextVehicle, false);
 					nextVehicle = vehicle;
+					if (enterCrossing) {
+						recorder.getCrossingCount().getInCrossingCount().decrementCount(vehicle.getType());
+						iterator.remove();
+						map.getCrossing().addVehicle(vehicle);
+					}
 				}
 			}
 
@@ -136,47 +233,115 @@ public class Updater implements ChangeListener<Number> {
 	}
 
 	public void draw() {
-		canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		map.draw(canvas.getGraphicsContext2D());
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		map.draw(gc);
 
 		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) { // for all roads
 			for (int j = 0; j < map.getNumberOfLanes(); j++) { // for all lanes
+				ListIterator<Vehicle> forwardIterator = map.getRoad(i).getForwardLane(j).listIterator();
+				while (forwardIterator.hasNext())
+					forwardIterator.next().draw(gc);
 
-				LinkedList<Vehicle> lane = roads[i].forwardLanes[j];
-				for (Vehicle vehicle : lane)
-					vehicle.draw(canvas.getGraphicsContext2D());
+				ListIterator<Vehicle> backwardIterator = map.getRoad(i).getBackwardLane(j).listIterator();
+				while (backwardIterator.hasNext())
+					backwardIterator.next().draw(gc);
+			}
+		}
 
+		ListIterator<Vehicle> crossingIterator = map.getCrossing().listIterator();
+		while (crossingIterator.hasNext())
+			crossingIterator.next().draw(gc);
+	}
+
+	private void generateVehicle(int sourceRoad, int sourceLane) {
+		Vehicle vehicle = null;
+
+		long carCount = recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getGeneratedCount()
+				.getCarCount();
+		long twoWheelerCount = recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getGeneratedCount()
+				.getTwoWheelerCount();
+		long heavyVehicleCount = recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getGeneratedCount()
+				.getHeavyVehicleCount();
+		double sum = carCount + twoWheelerCount + heavyVehicleCount;
+
+		if (MathEngine.isEqual(sum, 0)) {
+			sum = 1; // some dummy value;
+		}
+
+		double carFrac = config.getRoadProperty(sourceLane).getLaneProperty(sourceLane).getCarProbability()
+				- carCount / sum;
+		double twoWheelerFrac = config.getRoadProperty(sourceLane).getLaneProperty(sourceLane)
+				.getTwoWheelerProbability() - twoWheelerCount / sum;
+		double heavyVehicleFrac = config.getRoadProperty(sourceLane).getLaneProperty(sourceLane)
+				.getHeavyVehicleProbability() - heavyVehicleCount / sum;
+
+		double max = Math.max(Math.max(carFrac, twoWheelerFrac), heavyVehicleFrac);
+
+		if (MathEngine.isEqual(max, carFrac)) {
+			vehicle = this.getVehicle(sourceRoad, sourceLane, Type.CAR);
+			if (vehicle != null && map.getRoad(sourceRoad).getForwardLane(sourceLane).isEnoughSpace(vehicle)) {
+				map.getRoad(sourceRoad).getForwardLane(sourceLane).addVehicle(vehicle);
+				recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getGeneratedCount()
+						.incrementCarCount();
+				recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getinLaneCount().incrementCarCount();
+			}
+		} else if (MathEngine.isEqual(max, twoWheelerFrac)) {
+			vehicle = this.getVehicle(sourceRoad, sourceLane, Type.CAR);
+			if (vehicle != null && map.getRoad(sourceRoad).getForwardLane(sourceLane).isEnoughSpace(vehicle)) {
+				map.getRoad(sourceRoad).getForwardLane(sourceLane).addVehicle(vehicle);
+				recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getGeneratedCount()
+						.incrementTwoWheelerCount();
+				recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getinLaneCount()
+						.incrementTwoWheelerCount();
+			}
+
+		} else if (MathEngine.isEqual(max, heavyVehicleFrac)) {
+			vehicle = this.getVehicle(sourceRoad, sourceLane, Type.HEAVY_VEHICLE);
+			if (vehicle != null && map.getRoad(sourceRoad).getForwardLane(sourceLane).isEnoughSpace(vehicle)) {
+				map.getRoad(sourceRoad).getForwardLane(sourceLane).addVehicle(vehicle);
+				recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getGeneratedCount()
+						.incrementHeavyVehicleCount();
+				recorder.getRoadCount(sourceRoad).getForwardLaneCount(sourceLane).getinLaneCount()
+						.incrementHeavyVehicleCount();
 			}
 		}
 
 	}
 
-	private void generateVehicle() {
+	private Vehicle getVehicle(int roadIndex, int laneIndex, Type vehicleType) {
+		Road sourceRoad = map.getRoad(roadIndex);
+		ForwardLane sourceLane = sourceRoad.getForwardLane(laneIndex);
+		int destinationRoadIndex = ruleSet.getDestinationRoad(roadIndex, laneIndex);
+		int destinationLaneIndex = (int) Math.round(Math.random() * (map.getNumberOfLanes() - 1)); // randomly select
+																									// lane from 0 -2
+		Road destinationRoad = map.getRoad(destinationRoadIndex);
+		BackwardLane destinationLane = destinationRoad.getBackwardLane(destinationLaneIndex);
+		Vehicle vehicle;
+		switch (vehicleType) {
+		case CAR:
+			vehicle = vehicleManager.getCar(sourceRoad, sourceLane, destinationRoad, destinationLane, map.getCrossing(),
+					map.getWidthProperty(), map.getHeightProperty());
+			break;
 
-		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) { // for all roads
-			for (int j = 0; j < map.getNumberOfLanes(); j++) { // for all lanes
+		case TWO_WHEELER:
+			vehicle = vehicleManager.getTwoWheeler(sourceRoad, sourceLane, destinationRoad, destinationLane,
+					map.getCrossing(), map.getWidthProperty(), map.getHeightProperty());
+			break;
 
-				Vehicle vehicle = new Car(map.getRoad(i), map.getRoad(i).getForwardLane(j), null, null,
-						map.getCrossing(), map.getWidthProperty(), map.getHeightProperty());
-				this.setDestination(vehicle);
-				roads[i].forwardLanes[j].addFirst(vehicle);
+		case HEAVY_VEHICLE:
+			vehicle = vehicleManager.getHeavyVehicle(sourceRoad, sourceLane, destinationRoad, destinationLane,
+					map.getCrossing(), map.getWidthProperty(), map.getHeightProperty());
+			break;
 
-			}
+		default:
+			vehicle = null;
 		}
+
+		return vehicle;
 	}
 
 	public void computeVehicleRegions() {
-
-	}
-
-	private void setDestination(Vehicle vehicle) {
-		int roadIndex = ruleSet.getDestinationRoad(vehicle.getSourceRoad().getType(),
-				vehicle.getSourceLane().getIndex());
-		int laneIndex = (int) Math.round(Math.random() * (map.getNumberOfLanes() - 1)); // randomly select lane from 0 -
-																						// numberOfLanes -1
-
-		vehicle.setDestinationRoad(map.getRoad(roadIndex));
-		vehicle.setDestinationLane(map.getRoad(roadIndex).getBackwardLane(laneIndex));
 
 	}
 
@@ -185,6 +350,94 @@ public class Updater implements ChangeListener<Number> {
 		map.getWidthProperty().set(canvas.getWidth());
 		map.getHeightProperty().set(canvas.getHeight());
 
+	}
+
+	public void reset() {
+		clearVehicles();
+		recorder.reset();
+		isPaused = true;
+		isStopped = false;
+		initialized = false;
+
+	}
+
+	private void clearVehicles() {
+		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+			for (int j = 0; j < map.getNumberOfLanes(); j++) {
+				map.getRoad(i).getForwardLane(j).clearQueue();
+				map.getRoad(i).getBackwardLane(j).clearQueue();
+			}
+		}
+		map.getCrossing().clearQueue();
+	}
+
+	public boolean isInitialized() {
+		return this.initialized;
+	}
+
+	public boolean isStopped() {
+		return this.isStopped;
+	}
+
+	public boolean isPaused() {
+		return this.isPaused;
+	}
+
+	public void initialize() {
+		trafficTimer.startCountdown();
+		;
+		for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+			for (int j = 0; j < map.getNumberOfLanes(); j++) {
+				vehicleGenerationTimer[i][j].startCountdown();
+			}
+		}
+		this.isStopped = false;
+		this.isPaused = true;
+		initialized = true;
+	}
+
+	public void stop() {
+
+		if (isInitialized() && !isStopped()) {
+			timer.stop();
+			trafficTimer.stop();
+			trafficTimer.clearCountdown();
+			for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+				for (int j = 0; j < map.getNumberOfLanes(); j++) {
+					vehicleGenerationTimer[i][j].stop();
+					vehicleGenerationTimer[i][j].clearCountdown();
+				}
+			}
+			isStopped = true;
+		}
+	}
+
+	public void start() {
+
+		if (isInitialized() && !isStopped() && isPaused()) {
+			timer.start();
+			trafficTimer.start();
+			for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+				for (int j = 0; j < map.getNumberOfLanes(); j++) {
+					vehicleGenerationTimer[i][j].start();
+				}
+			}
+			isPaused = false;
+		}
+	}
+
+	public void pause() {
+
+		if (isInitialized() && !isStopped() && !isPaused()) {
+			timer.stop();
+			trafficTimer.stop();
+			for (int i = 0; i < Map.NUMBER_OF_ROADS; i++) {
+				for (int j = 0; j < map.getNumberOfLanes(); j++) {
+					vehicleGenerationTimer[i][j].stop();
+				}
+			}
+			isPaused = true;
+		}
 	}
 
 }
